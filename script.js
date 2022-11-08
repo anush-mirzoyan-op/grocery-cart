416e2339e7fc71d7990ac3465967065aec225292
mirzoyananush <anush.mirzoyan@optimumpartners.co>
2022-11-08 14:43:57 +0400

    add function to shopping-icon, so when you click it adds sweet's price to balance

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-window.onload = () => {
-  console.log('loadded');
-}
-
 import {data} from "./data.js";
 
-console.log(data);
+let cartBalance = document.getElementById('cartBalance')
 
-window.createCard = ({ imageLink, candyName: name, cndyPrice: price })=> {
+window.createCard = ({ imageLink, candyName: name, price }) => {
   const card = document.createElement("div");
   card.classList.add("card");
 
@@ -22,6 +18,7 @@ window.createCard = ({ imageLink, candyName: name, cndyPrice: price })=> {
   const shoppingIcon = document.createElement("i");
   shoppingIcon.classList.add("fas", "fa-shopping-cart", "card-shopping-icon");
   hoveringImage.appendChild(shoppingIcon);
+  shoppingIcon.addEventListener('click' , ()=>addToCart(price))
 
   let priceContainer = document.createElement("div");
   priceContainer.classList.add("sweet-item-price");
@@ -32,12 +29,12 @@ window.createCard = ({ imageLink, candyName: name, cndyPrice: price })=> {
   priceContainer.appendChild(candyName);
 
   let candyPrice = document.createElement("strong");
-  candyPrice.innerHTML = price;
+  candyPrice.innerHTML = "$" + price;
   priceContainer.appendChild(candyPrice);
   document.querySelector('#grid-container').appendChild(card)
 }
 
-window.renderStoreData = (type)=> {
+window.renderStoreData = (type) => {
   document.querySelector('#grid-container').innerHTML = ''
   data.map((item) => {
     if (type == "All" || item.type == type) {
@@ -46,3 +43,9 @@ window.renderStoreData = (type)=> {
     }
   });
 }
+
+window.addToCart = (val) => {
+  console.log(cartBalance.innerHTML)
+  console.log(val)
+  cartBalance.innerHTML = ( +cartBalance.innerHTML + val ).toFixed(2)
+ }
