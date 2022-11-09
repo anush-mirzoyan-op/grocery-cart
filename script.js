17d3660c87661edce20bf83628ec35ec085c3976
mirzoyananush <anush.mirzoyan@optimumpartners.co>
2022-11-09 17:03:46 +0400

    work on dropdown menu, so when you click on shopping-icon - it adds item image to dropdown list

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-import {data} from "./data.js";
+import { data } from "./data.js";
 
-let cartBalance = document.getElementById('cartBalance')
+let cartBalance = document.getElementById("cartBalance");
+let balanceButton = document.getElementById("openDropdown");
+
+
+balanceButton.addEventListener("click", () => createDropdown());
 
 window.createCard = ({ imageLink, candyName: name, price }) => {
   const card = document.createElement("div");
@@ -18,7 +22,7 @@ window.createCard = ({ imageLink, candyName: name, price }) => {
   const shoppingIcon = document.createElement("i");
   shoppingIcon.classList.add("fas", "fa-shopping-cart", "card-shopping-icon");
   hoveringImage.appendChild(shoppingIcon);
-  shoppingIcon.addEventListener('click' , ()=>addToCart(price))
+  shoppingIcon.addEventListener("click", () => addToCartandDropdown(price ,imageLink)); //ֆունկցիային տալիս ենք նաև նկարի լինքը որ միանգամից դռոփդաունի մեջ նկարի
 
   let priceContainer = document.createElement("div");
   priceContainer.classList.add("sweet-item-price");
@@ -31,21 +35,39 @@ window.createCard = ({ imageLink, candyName: name, price }) => {
   let candyPrice = document.createElement("strong");
   candyPrice.innerHTML = "$" + price;
   priceContainer.appendChild(candyPrice);
-  document.querySelector('#grid-container').appendChild(card)
-}
+  document.querySelector("#grid-container").appendChild(card);
+};
 
 window.renderStoreData = (type) => {
-  document.querySelector('#grid-container').innerHTML = ''
+  document.querySelector("#grid-container").innerHTML = "";
   data.map((item) => {
     if (type == "All" || item.type == type) {
       createCard(item);
-      console.log('create card');
+      console.log("create card");
     }
   });
-}
+};
 
-window.addToCart = (val) => {
-  console.log(cartBalance.innerHTML)
-  console.log(val)
-  cartBalance.innerHTML = ( +cartBalance.innerHTML + val ).toFixed(2)
- }
+const dropDown = document.createElement('div')
+dropDown.classList.add(".dropdown-content");
+
+document.querySelector("#openDropdown").appendChild(dropDown);
+
+
+window.addToCartandDropdown = (val , imagelink) => {
+  console.log(cartBalance.innerHTML);
+  console.log(val);
+  console.log(imagelink);
+
+  const dropdownItems = document.createElement('div')
+  dropdownItems.innerHTML = `<img src = ${imagelink}>`
+  dropdownItems.classList.add("dropdownItem")
+  dropDown.appendChild(dropdownItems)
+  cartBalance.innerHTML = (+cartBalance.innerHTML + val).toFixed(2);
+};
+
+
+//սարքում ենք սկզբի համար էդ Dropdown-ը ու կպցնում ենք nav-bar-ին
+window.createDropdown = () => {
+  console.log("created")
+};
